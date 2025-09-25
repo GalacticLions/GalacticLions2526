e88f2aea67c3945fbba02cbe28580b91b896b395
Brody60 <brody.linder@lakeviewacademy.org>
2025-09-25 15:27:08 -0400

    Update StarterBotTeleop.java
    Lowered LAUCHER_TARGET_VELOCITY from 1250 to 975 & lowered LAUNCHER_MIN_VELOCITY from 1100 to 875

--- Team26248/src/main/java/org/firstinspires/ftc/team26248/StarterBotTeleop.java
+++ Team26248/src/main/java/org/firstinspires/ftc/team26248/StarterBotTeleop.java
@@ -71,8 +71,8 @@ public class StarterBotTeleop extends OpMode {
      * velocity. Here we are setting the target, and minimum velocity that the launcher should run
      * at. The minimum velocity is a threshold for determining when to fire.
      */
-    final double LAUNCHER_TARGET_VELOCITY = 1250;
-    final double LAUNCHER_MIN_VELOCITY = 1100;
+    final double LAUNCHER_TARGET_VELOCITY = 975;
+    final double LAUNCHER_MIN_VELOCITY = 875;
 
     // Declare OpMode members.
     private DcMotor leftDrive = null;

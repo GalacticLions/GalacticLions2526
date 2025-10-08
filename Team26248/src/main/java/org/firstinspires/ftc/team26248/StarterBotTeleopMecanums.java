031c23cd82a239ff970f8c97648c8e328f2d0e52
Brody60 <brody.linder@lakeviewacademy.org>
2025-10-08 15:25:48 -0400

    Update StarterBotTeleopMecanums.java
    Fixed new number for launcher with the new addon. fix the servo

--- Team26248/src/main/java/org/firstinspires/ftc/team26248/StarterBotTeleopMecanums.java
+++ Team26248/src/main/java/org/firstinspires/ftc/team26248/StarterBotTeleopMecanums.java
@@ -71,8 +71,8 @@ public class StarterBotTeleopMecanums extends OpMode {
      * velocity. Here we are setting the target, and minimum velocity that the launcher should run
      * at. The minimum velocity is a threshold for determining when to fire.
      */
-    final double LAUNCHER_TARGET_VELOCITY = 1050;
-    final double LAUNCHER_MIN_VELOCITY = 950;
+    final double LAUNCHER_TARGET_VELOCITY = 1250;
+    final double LAUNCHER_MIN_VELOCITY = 1150;
 
     // Declare OpMode members.
     private DcMotor leftFrontDrive = null;

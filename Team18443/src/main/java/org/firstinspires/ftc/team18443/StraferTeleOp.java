9c10877af4811efbeaa5928cb2b02e38cd66985d
mikhaillovell <34150685+mikhaillovell@users.noreply.github.com>
2025-10-14 15:04:55 -0400

    Update StraferTeleOp.java

--- Team18443/src/main/java/org/firstinspires/ftc/team18443/StraferTeleOp.java
+++ Team18443/src/main/java/org/firstinspires/ftc/team18443/StraferTeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamm18443;
+package org.firstinspires.ftc.team18443;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;

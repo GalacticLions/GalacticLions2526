39aa54307b04197df4baeec9863a984c0c476060
HackingU0 <eli.xiao@lakeviewacademy.org>
2026-01-28 17:03:30 -0500

    Red Alliance Pedropathing fix (Poses need to be worked on)

--- Team5898/src/main/java/org/firstinspires/ftc/team5898/pedroPathing/Constants.java
+++ Team5898/src/main/java/org/firstinspires/ftc/team5898/pedroPathing/Constants.java
@@ -6,7 +6,6 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
-import com.pedropathing.ftc.localization.Encoder;
 import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;

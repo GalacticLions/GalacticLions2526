c0dfcda9c730a386fda592c2e6a6e1b885e4073d
Eli <xiaozhiqi572@gmail.com>
2026-02-11 21:01:43 -0500

    New TeleOP with FSM

--- Team5898/src/main/java/org/firstinspires/ftc/team5898/Beta_TeleOP_Refactor.java
+++ Team5898/src/main/java/org/firstinspires/ftc/team5898/Beta_TeleOP_Refactor.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.team5898;
 
-
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -27,42 +26,35 @@ public class Beta_TeleOP_Refactor extends OpMode {
     Servo stopperServo;
     DcMotor frontIntake;
     IMU imu;
-    Double intakePower;
-    Double LAUNCH_VELOCITY;
-    int kP;
-    Double kI;
-    Double kD;
-    Integer kF;
 
-    enum States {IDLE, INTAKE, LAUNCH, SPOOL_UP, VISUAL_SERVOING};
+    double intakePower;
+    double LAUNCH_VELOCITY;
+    double kP, kI, kD, kF;
+
+    enum States {IDLE, INTAKE, LAUNCH, SPOOL_UP, VISUAL_SERVOING}
     private States robotState = States.IDLE;
-    boolean a1Pressed = gamepad1.a, b2Pressed = gamepad2.b, a2Pressed = gamepad2.a;
-    boolean prevA1 = a1Pressed, prevB2 = b2Pressed, prevA2 = a2Pressed;
-    boolean a1JustPressed = a1Pressed && !prevA1, b2JustPressed = b2Pressed && !prevB2, a2JustPressed = a2Pressed && !prevA2;
 
-
+    // Button state variables
+    boolean prevA1 = false, prevB2 = false, prevA2 = false;
 
     @Override
     public void init() {
-        //Constants Init
-        //Cannon Constants
+        // Constants Init
         intakePower = CannonConstants.IntakePower;
         kP = CannonConstants.kP;
         kI = CannonConstants.kI;
         kD = CannonConstants.kD;
         kF = CannonConstants.kF;
-
-        // Set target velocities in Ticks Per Second. These are example values and will need tuning.
         LAUNCH_VELOCITY = CannonConstants.LAUNCH_VELOCITY;
 
-        //Limelight Init
+        // Limelight Init
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.pipelineSwitch(0);
         telemetry.setMsTransmissionInterval(11);
         limelight.setPollRateHz(90);
         limelight.start();
 
-        //Motors Init
+        // Motors Init
         frontLeft = hardwareMap.get(DcMotor.class, "FL");
         frontRight = hardwareMap.get(DcMotor.class, "FR");
         backLeft = hardwareMap.get(DcMotor.class, "BL");
@@ -71,14 +63,16 @@ public class Beta_TeleOP_Refactor extends OpMode {
         bottomLauncher = hardwareMap.get(DcMotorEx.class, "BLaunch");
         frontIntake = hardwareMap.get(DcMotor.class, "Intake");
 
-
+        // Directions
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD); // Added missing direction
 
         topLauncher.setDirection(DcMotorSimple.Direction.FORWARD);
         bottomLauncher.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        // Mode setup
         topLauncher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         bottomLauncher.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         topLauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -100,183 +94,171 @@ public class Beta_TeleOP_Refactor extends OpMode {
 
         frontIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //IMU Init for Field-Centric
+        // IMU Init
         imu = hardwareMap.get(IMU.class, "imu");
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP));
         imu.initialize(parameters);
 
-        //Visual Servoing Init
+        // Visual Servoing Init
         visualServoing = new VisualServoing(limelight, frontLeft, frontRight, backRight, backLeft, telemetry);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
     }
 
     @Override
     public void loop() {
-        kP = CannonConstants.kP;
-        kI = CannonConstants.kI;
-        kD = CannonConstants.kD;
+        // 1. Update button states
+        boolean a1Pressed = gamepad1.a;
+        boolean b2Pressed = gamepad2.b;
+        boolean a2Pressed = gamepad2.a;
+        boolean rightTrigger2 = gamepad2.right_trigger > 0.3; // Threshold for trigger
 
+        boolean a1JustPressed = a1Pressed && !prevA1;
+        boolean b2JustPressed = b2Pressed && !prevB2;
+        boolean a2JustPressed = a2Pressed && !prevA2;
+
+        // 2. State machine logic
         switch (robotState) {
             case IDLE:
                 FieldCentricDrive(false);
-                if (a1JustPressed) {
-                    robotState = States.VISUAL_SERVOING;
-                }
+
+                // Stop all motors
                 backLeftServo.setPower(0);
                 backRightServo.setPower(0);
                 frontIntake.setPower(0);
-                bottomLauncher.setPower(0);
                 topLauncher.setPower(0);
-                if(b2JustPressed){
-                    robotState = States.INTAKE;
-                }
+                bottomLauncher.setPower(0);
+                stopperServo.setPosition(1); // Close the stopper
 
-                stopperServo.setPosition(1); //TODO: Can be changed
+                // State transitions
+                if (b2JustPressed) {
+                    robotState = States.INTAKE;
+                } else if (a1JustPressed) {
+                    robotState = States.VISUAL_SERVOING;
+                }
+                break;
 
             case INTAKE:
                 FieldCentricDrive(false);
-                if(b2JustPressed){
-                    robotState = States.IDLE;
-                }
 
-                if(gamepad2.right_trigger > 0.3){
-                    robotState = States.SPOOL_UP;
-                }
-
+                // Intake action
                 backLeftServo.setPower(intakePower);
                 backRightServo.setPower(intakePower);
-                frontIntake.setPower(.7);
-                stopperServo.setPosition(0.5); //TODO: Can be changed
+                frontIntake.setPower(0.7);
+                stopperServo.setPosition(1); // Ensure stopper is closed
 
-            case LAUNCH:
-                FieldCentricDrive(false);
-                backLeftServo.setPower(.7);
-                topLauncher.setVelocity(LAUNCH_VELOCITY);
-                bottomLauncher.setVelocity(LAUNCH_VELOCITY);
-                if () {
-
+                // State transitions
+                if (b2JustPressed) {
+                    robotState = States.IDLE; // Press B again to stop
+                } else if (rightTrigger2) {
+                    robotState = States.SPOOL_UP; // Press Trigger to start spooling up
                 }
-
+                break;
 
             case SPOOL_UP:
                 FieldCentricDrive(false);
-                if(gamepad2.right_trigger > 0.3){
-                robotState = States.SPOOL_UP;
-            } else if (gamepad2.right_trigger < 0.3){
-                robotState = States.IDLE;
-            } if () {
+
+                // Launcher wheel acceleration
                 topLauncher.setVelocity(LAUNCH_VELOCITY);
                 bottomLauncher.setVelocity(LAUNCH_VELOCITY);
-                }
 
+                // Optional: keep intake running or stop. Here we stop intake while waiting to launch to prevent jamming
+                backLeftServo.setPower(0);
+                backRightServo.setPower(0);
+                frontIntake.setPower(0);
+                stopperServo.setPosition(1); // Stopper closed
+
+                // State transitions
+                if (!rightTrigger2) {
+                    robotState = States.IDLE; // Release Trigger to return to IDLE
+                } else if (a2JustPressed) {
+                    robotState = States.LAUNCH; // Press A to launch
+                }
+                break;
+
+            case LAUNCH:
+                FieldCentricDrive(false);
+
+                // Maintain launcher wheel velocity
+                topLauncher.setVelocity(LAUNCH_VELOCITY);
+                bottomLauncher.setVelocity(LAUNCH_VELOCITY);
+
+                // Open stopper and feed the ring
+                stopperServo.setPosition(0.5); // Move stopper away
+                backLeftServo.setPower(0.7);   // Feed servos/motors
+                backRightServo.setPower(0.7);
+
+                // State transitions
+                // If A is released, return to SPOOL_UP (keep spinning, prep for next shot)
+                if (!a2Pressed) {
+                    robotState = States.SPOOL_UP;
+                }
+                // If Trigger is released, return to IDLE
+                if (!rightTrigger2) {
+                    robotState = States.IDLE;
+                }
+                break;
 
             case VISUAL_SERVOING:
                 FieldCentricDrive(true);
                 visualServoing.visualServo();
+
+                // State transitions
                 if (a1JustPressed) {
                     robotState = States.IDLE;
                 }
-
+                break;
         }
 
-        //VisualServoing: gamepad1.a
-        //Check if VisualServoing
-        boolean isVisualServoing = gamepad1.a;
-        if (isVisualServoing) {
-            visualServoing.visualServo();
-        }
+        // 3. Update previous button states
+        prevA1 = a1Pressed;
+        prevB2 = b2Pressed;
+        prevA2 = a2Pressed;
 
-        //Cannon Control - Now using setVelocity for closed-loop control
-        if (gamepad2.left_stick_y > 0.3) {
-            topLauncher.setVelocity(-LAUNCH_VELOCITY);
-            bottomLauncher.setVelocity(LAUNCH_VELOCITY);
-        } else if (gamepad2.left_stick_y < -0.3) {
-            topLauncher.setVelocity(LAUNCH_VELOCITY);
-            bottomLauncher.setVelocity(-LAUNCH_VELOCITY);
-        } else {
-            topLauncher.setVelocity(0);
-            bottomLauncher.setVelocity(0);
-        }
-
-        if (gamepad2.right_stick_y > 0.3) {
-            backLeftServo.setPower(-intakePower);
-            backRightServo.setPower(-intakePower);
-            frontIntake.setPower(-intakePower);
-        } else if (gamepad2.right_stick_y < -0.3) {
-            backLeftServo.setPower(intakePower);
-            backRightServo.setPower(intakePower);
-            frontIntake.setPower(.7);
-        }else {
-            backLeftServo.setPower(0);
-            backRightServo.setPower(0);
-            frontIntake.setPower(0);
-        }
-
-        if (gamepad2.dpad_down){
-            backLeftServo.setPower(intakePower-0.3);
-            backRightServo.setPower(intakePower-0.3);
-        }else if (gamepad2.dpad_up){
-            backLeftServo.setPower(-intakePower+0.3);
-            backRightServo.setPower(-intakePower+0.3);
-        }
-
-
-
-
+        // Telemetry
+        telemetry.addData("State", robotState);
+        telemetry.addData("Launcher Velocity", topLauncher.getVelocity());
+        telemetry.update();
     }
-    public void FieldCentricDrive(Boolean isVisualServoing){
+
+    public void FieldCentricDrive(Boolean isVisualServoing) {
         if (!isVisualServoing) {
-            double y = -gamepad1.left_stick_y; // Forward/Backward (reversed)
-            double x = gamepad1.left_stick_x * 1.1; // Strafe Left/Right (counteract imperfect strafing)
-            double rx = gamepad1.right_stick_x; // Rotation
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
 
-
-            // Reset IMU yaw with guide button
             if (gamepad1.guide) {
                 imu.resetYaw();
             }
 
-            // Get robot heading from IMU
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
 
-            // Rotate movement direction based on robot's heading
             double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
             double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
-            // Calculate motor powers
             double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
             double frontLeftPower = (rotY + rotX + rx) / denominator;
             double backLeftPower = (rotY - rotX + rx) / denominator;
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
-            if (!gamepad1.left_bumper) {
-                // Set motor powers (adjust multiplier as needed)
-                frontLeft.setPower(frontLeftPower);
-                backLeft.setPower(backLeftPower);
-                frontRight.setPower(frontRightPower);
-                backRight.setPower(backRightPower);
-            } else {
-                frontLeft.setPower(frontLeftPower * 0.5);
-                backLeft.setPower(backLeftPower * 0.5);
-                frontRight.setPower(frontRightPower * 0.5);
-                backRight.setPower(backRightPower * 0.5);
-            }
+
+            double multiplier = gamepad1.left_bumper ? 0.5 : 1.0;
+
+            frontLeft.setPower(frontLeftPower * multiplier);
+            backLeft.setPower(backLeftPower * multiplier);
+            frontRight.setPower(frontRightPower * multiplier);
+            backRight.setPower(backRightPower * multiplier);
         }
+        // If isVisualServoing is true, this method does nothing, chassis is handled by visualServoing.visualServo()
     }
+
     @Override
-    public void stop(){
+    public void stop() {
         limelight.stop();
-        leftSlide.setPower(0);
-        rightSlide.setPower(0);
-    }
-    public void setIntakePower(double power) {
-        frontIntake.setPower(0);
-        backLeftServo.setPower(0);
-        backRightServo.setPower(0);
-        topLauncher.setVelocity(0);
-        bottomLauncher.setVelocity(0);
+        if (leftSlide != null) leftSlide.setPower(0);
+        if (rightSlide != null) rightSlide.setPower(0);
     }
 }
